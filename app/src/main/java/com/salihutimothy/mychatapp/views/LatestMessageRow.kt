f186b72472331748f125817159907a84ab532a71
DrTim105 <Salihutimothy52@gmail.com>
2021-12-20 23:33:13 +0100

    updated code - add latest messages to view

--- app/src/main/java/com/salihutimothy/mychatapp/views/LatestMessageRow.kt
+++ app/src/main/java/com/salihutimothy/mychatapp/views/LatestMessageRow.kt
@@ -1,16 +1,50 @@
 package com.salihutimothy.mychatapp.views
 
+import android.widget.ImageView
 import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.salihutimothy.mychatapp.R
 import com.salihutimothy.mychatapp.models.ChatMessage
+import com.salihutimothy.mychatapp.models.User
+import com.squareup.picasso.Picasso
 import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
 
-class LatestMessageRow(val chatMessage: ChatMessage): Item<ViewHolder>() {
+class LatestMessageRow(val chatMessage: ChatMessage) : Item<ViewHolder>() {
+    var chatPartnerUser: User? = null
+
     override fun bind(viewHolder: ViewHolder, position: Int) {
-        val latestMessage = viewHolder.itemView.findViewById<TextView>(R.id.message_textview_latest_message)
+        val latestMessage =
+            viewHolder.itemView.findViewById<TextView>(R.id.message_textview_latest_message)
+        val chatName =
+            viewHolder.itemView.findViewById<TextView>(R.id.username_textview_new_message)
+        val chatImage =
+            viewHolder.itemView.findViewById<ImageView>(R.id.imageview_latest_message)
 
         latestMessage.text = chatMessage.text
+//        val chatPartnerId: String = if (chatMessage.fromId == FirebaseAuth.getInstance().uid) {
+//            chatMessage.toId
+//        } else {
+//            chatMessage.fromId
+//        }
+//
+//        val ref = FirebaseDatabase.getInstance().getReference("/users/$chatPartnerId")
+//        ref.addListenerForSingleValueEvent(object : ValueEventListener {
+//            override fun onDataChange(p0: DataSnapshot) {
+//                chatPartnerUser = p0.getValue(User::class.java)
+//                chatName.text = chatPartnerUser?.username
+//
+//                Picasso.get().load(chatPartnerUser?.profileImageUrl).into(chatImage)
+//            }
+//
+//            override fun onCancelled(p0: DatabaseError) {
+//
+//            }
+//        })
     }
 
     override fun getLayout(): Int {
